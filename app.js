c60f8d2dbe6ca326900c480b9f14ec6163fcebdc
Angel Mendez <angeldavidmendez2012@gmail.com>
2023-08-15 20:50:55 -0600

    adding books using form

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-let titles = document.getElementsByClassName("title");
+const list = document.querySelector("#book-list ul");
 
-Array.from(titles).forEach(function (item) {
-  console.log(item);
+//delete books
+list.addEventListener("click", (e) => {
+  if (e.target.className == "delete") {
+    const li = e.target.parentElement;
+    list.removeChild(li);
+  }
+});
+
+// add books
+const addForm = document.forms["add-book"];
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+
+  // create elments
+  const li = document.createElement("li");
+  const bookName = document.createElement("span");
+  const deleteBtn = document.createElement("span");
+
+  //add content
+  deleteBtn.textContent = "delete";
+  bookName.textContent = value;
+
+  //add classes
+  bookName.classList.add("name");
+  deleteBtn.classList.add("delete");
+
+  // apend to document
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
