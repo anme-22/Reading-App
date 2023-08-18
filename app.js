6745befb8b7981f15e34c81efce71dc6dca8666e
Angel Mendez <angeldavidmendez2012@gmail.com>
2023-08-17 19:47:10 -0600

    finish the table

--- app.js
+++ app.js
@@ -33,3 +33,28 @@ addForm.addEventListener("submit", (e) => {
   li.appendChild(deleteBtn);
   list.appendChild(li);
 });
+
+// hide books
+
+const hideBox = document.querySelector("#hide");
+hideBox.addEventListener("change", () => {
+  if (hideBox.checked) {
+    list.style.display = "none";
+  } else {
+    list.style.display = "block";
+  }
+});
+
+const searchBar = document.forms["search-books"].querySelector("input");
+searchBar.addEventListener("keyup", (e) => {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName("li");
+  Array.from(books).forEach(function jo(book) {
+    const title = book.firstChild.textContent;
+    if (title.toLowerCase().indexOf(term) != -1) {
+      book.style.display = "block";
+    } else {
+      book.style.display = "none";
+    }
+  });
+});
